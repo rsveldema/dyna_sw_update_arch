9a9953afb981551d7768483557639f9ecf621705
Ronald Veldema <rsveldema@gmail.com>
2021-06-26 15:42:10 +0000

    add initial setup

---
+++ lib/dyn_update.h
@@ -0,0 +1,18 @@
+#ifndef DYNA_UPDATER___H____
+#define DYNA_UPDATER___H____
+
+namespace DUpdate
+{
+
+    class IMemory
+    {
+    };
+
+
+    class IFunction
+    {        
+    };
+}
+
+
+#endif
